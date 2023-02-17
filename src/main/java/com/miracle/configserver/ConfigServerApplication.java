8c799a41a0d8fbfcc676e9d7c243adc9816da352
DurgaRaoGopu <117093141+DurgaRaoGopu@users.noreply.github.com>
2023-02-17 17:37:07 +0530

    Update ConfigServerApplication.java

--- src/main/java/com/miracle/configserver/ConfigServerApplication.java
+++ src/main/java/com/miracle/configserver/ConfigServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
 @EnableConfigServer
-
+@EnableEurekaServer
 @EnableAutoConfiguration
 public class ConfigServerApplication {
 
